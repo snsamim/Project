6ad2f6e41bd01d78c1f3926e02df03f1d4ab13fc
snsamim <shoaibnurainsamim@gmail.com>
2025-02-03 17:14:38 +0600

    Initilize
    initilize all class

---
+++ Hotel_Management_System/Billing.java
@@ -0,0 +1,5 @@
+package Hotel_Management_System;
+
+public class Billing {
+    
+}
---
+++ Hotel_Management_System/Room.java
@@ -0,0 +1,41 @@
+package Hotel_Management_System;
+
+// Abstract Room class using inheritance
+abstract class Room {
+    private int roomNumber;
+    private String type;
+    private double pricePerDay;
+
+    public Room(int roomNumber, String type, double pricePerDay) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.pricePerDay = pricePerDay;
+    }
+
+    // Getters
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    static class StandardRoom extends Room {
+        public StandardRoom(int roomNumber, double pricePerDay) {
+            super(roomNumber, "Standard", pricePerDay);
+        }
+    }
+
+    static class DeluxeRoom extends Room {
+        public DeluxeRoom(int roomNumber, double pricePerDay) {
+            super(roomNumber, "Deluxe", pricePerDay);
+        }
+    }
+}
+
+
