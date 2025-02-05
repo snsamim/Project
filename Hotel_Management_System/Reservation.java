a90f7d976a8c117018aa03e6621d53b6a2b01377
snsamim <shoaibnurainsamim@gmail.com>
2025-02-05 21:47:48 +0600

    generate Reservation Class and modify

--- Hotel_Management_System/Reservation.java
+++ Hotel_Management_System/Reservation.java
@@ -1,5 +1,34 @@
 package Hotel_Management_System;
+import java.time.LocalDate;
 
-public class Reservation {
-    
+class Reservation {
+    private Room room;
+    private Customer customer;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    public Reservation(Room room, Customer customer, LocalDate checkInDate, LocalDate checkOutDate) {
+        this.room = room;
+        this.customer = customer;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    // Getters
+    public Room getRoom() {
+        return room;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
 }
+
