9af0483d9216e411692fe7bf1dbdaedb2f3950fa
snsamim <shoaibnurainsamim@gmail.com>
2025-02-05 21:34:24 +0600

    Create and Encapsulate the customar class

--- Hotel_Management_System/Customer.java
+++ Hotel_Management_System/Customer.java
@@ -1,5 +1,40 @@
 package Hotel_Management_System;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
-    
+    private int id;
+    private String name;
+    private String contactInfo;
+    private List<Reservation> bookingHistory;
+
+    public Customer(int id, String name, String contactInfo) {
+        this.id = id;
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.bookingHistory = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        bookingHistory.add(reservation);
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public List<Reservation> getBookingHistory() {
+        return bookingHistory;
+    }
 }
+
